fd4341a983bc7ddeb4c7917263c5e7f1742d10cb
Student <julian.cui.2030@huilieducation.cn>
2025-07-31 15:48:20 -0700

    add modelLD

--- python project.py
+++ python project.py
@@ -1,1 +1,30 @@
-sss 
+#!/usr/bin/python3
+
+import jetson_inference
+import jetson_utils
+
+import argparse
+
+from numpy.distutils.lib2def import output_def
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", type=str, help="filename of the image to process")
+parser.add_argument("--network", type=str, default="resenet-18", help="model to use, can be:  googlenet, resnet-18, ect. (see --help for others)")
+opt = parser.parse_args()
+
+img = jetson_utils.loadImage(opt.filename)
+
+net = jetson_inference.imageNet(
+    opt.network,
+    model="resenet18.onnx",
+    input_blob="input_0",
+    output_blob="output_o",
+    labels="dataset/labels.txt",
+
+         )
+
+class_idx, confidence = net.Classify(img)
+
+class_desc = net.GetClassDesc(class_idx)
+
+print("image is recognized as "+ str(class_desc) +" (class #"+ str(class_idx) +") with " + str(confidence*100)+"% confidence"
